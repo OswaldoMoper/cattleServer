a71bf23f2005b0235e1e2df3a7149dd75e34cbc0
Oswaldo Moper <65831229+OswaldoMoper@users.noreply.github.com>
2024-02-16 15:17:47 -0600

    Add database backup (#1)
    * Start ignoring cattleServer.cabal
    
    * Add function to connect and backup to server
    
    * Refactor package.yaml and flake.nix
    
    * Add ssh functions in Main
    
    * Add simplessh to the dependencies
    
    * Remove Main --Result
    
    * Comment simplessh to build test
    
    * Add libssh2, cabal-pkg-config and hs-pkg-config to build test
    
    * Add pkgs in flake to build test
    
    * Add 'haskellPackages.'
    
    * Remove hs-pkg-config
    
    * Add libssh2 to the stack packages from nix
    
    * Remove libssh2 from package.yaml
    
    * Prepare for testing and debugging phase
    
    * Update simplessh to the jesseDMoore version
    
    * SSH Conection Test
    
    * Remove unused packages in flake
    
    * Remove unused commits
    
    * Add database backup ssh action
    
    * Hide System.Exit.ExitFailure
    
    * Add closeSession code
    
    * Refactor
    
    * Remove unused dependencies

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,52 @@
 module Main where
 
-import           Control.Applicative
-import           Control.Exception
-import           Control.Monad
-import           Control.Monad.IO.Class
+-- import           Data.Text
+import           Network.SSH.Client.SimpleSSH
+import           System.Exit                  hiding (ExitFailure)
+import           System.IO
 import           System.Process
-import           Yesod.Core.Types
+
+path :: String
+path = "/home/<remote-user>"
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  loginResponse <- runSimpleSSH loginToServer
+  case loginResponse of
+    Left err      -> do
+      hPutStrLn stderr $ "Error: " ++ show err
+      exitFailure
+    Right session -> do
+      putStrLn "SSH connection started"
+      commandResponse <- runSimpleSSH $ databaseBackupInServer session
+      case commandResponse of
+        Left err     -> do
+          hPutStrLn stderr $ "Error: " ++ show err
+          exitFailure
+        Right result -> do
+          printResponse result "yesod-project.sql created successfully"
+          closeResponse <- runSimpleSSH $ closeSession session
+          case closeResponse of
+            Left err -> do
+              hPutStrLn stderr $ "Error: " ++ show err
+              exitFailure
+            Right () -> do
+              putStrLn "SSH connection closed"
 
-writeLocally :: String -> String -> FileInfo -> Handler FilePath
-writeLocally path name file = do
-  let filename = path ++ "/" ++ name
-  liftIO $ callCommand $ "mkdir" ++ path
-  liftIO $ fileMove file filename
-  return filename
+loginToServer :: SimpleSSH Session
+loginToServer = do
+  session <- openSession "0.0.0.0" 22 "/home/<user>/.ssh/known_hosts"
+  auth_session <- authenticateWithKey session "<remote-user>" "/home/<user>/.ssh/exampleKey-ed25519.pub" "/home/<user>/.ssh/exampleKey-ed25519" ""
+  return auth_session
+
+databaseBackupInServer :: Session -> SimpleSSH Result
+databaseBackupInServer session = do
+  response <- execCommand session $ "pg_dump -U postgres yesod-project > " <> path <> "/backup/yesod-project.sql"
+  return response
+
+printResponse :: Result -> String -> IO ()
+printResponse res success = do
+  case resultExit res of
+    ExitFailure 1 -> putStrLn $ show (resultErr res)
+    _               -> do
+      putStrLn $ show (resultExit res) <> ": " <> success -- resultExit must not print an error
